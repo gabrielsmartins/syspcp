0eab839aaf917e186a9cd116443d4ccf5e81fb29
Gabriel Martins <ga.smartins94@gmail.com>
2017-12-12 20:19:01 -0200

    Sessão OK

--- src/main/java/br/ifsp/edu/pcp/model/Sessao.java
+++ src/main/java/br/ifsp/edu/pcp/model/Sessao.java
@@ -1,25 +1,33 @@
 package br.ifsp.edu.pcp.model;
 
+import java.io.Serializable;
+
 import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
 
 import br.ifsp.edu.pcp.model.Usuario;
 
-
 @SessionScoped
-public class Sessao {
-	
+@Named("session")
+public class Sessao implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	private Usuario usuario;
-	
-	 public void login(Usuario usuario) {
-		    this.usuario = usuario;
-		  }
-		  
-		  public String getNome() {
-		    return usuario.getNome();
-		  }
-		  
-		  public boolean isLogado() {
-		    return this.usuario != null;
-		  }
+
+	public void login(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public boolean isLogado() {
+		return this.usuario != null;
+	}
 
 }
--- src/main/webapp/WEB-INF/jsp/main/home.jsp
+++ src/main/webapp/WEB-INF/jsp/main/home.jsp
@@ -10,7 +10,7 @@
 	 <%@ include file="../common/sidebar.jspf"%>
 	 <%@ include file="../common/navbar.jspf"%>
 	
-	<h1>Olá ${sessao.usuario.nome} </h1>
+	<h1>Olá ${session.usuario.nome} </h1>
 
 	</main>
 </body>
